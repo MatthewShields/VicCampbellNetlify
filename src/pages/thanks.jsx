602fb4a16af9631cc33d9ec9bb73c3f372deb0c3
MatthewShields <info@matthewshields.co.uk>
2020-05-09 07:56:28 +0100

    add contact form thanks page

---
+++ src/pages/thanks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../layout";
+import SEO from "../components/SEO/SEO";
+import Hero from "../components/Hero/Hero";
+
+export default class PostTemplate extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1 className="text-4xl text-center mb-6">Thanks!</h1>
+          <div className="max-w-screen-md text-center mx-auto mb-16">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At culpa nulla veritatis beatae ullam quas repellendus praesentium corporis deserunt ab porro alias debitis voluptatum, dignissimos adipisci, dolor laborum minus hic!</p>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
